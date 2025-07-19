6a0fa4ec97f77cad46c6b8926ae3bd8e30192fc2
mbero_akoko <mberoakoko@gmail.com>
2025-07-20 00:28:30 +0200

    we can now draw trajectories of Quatanions from disk

--- include/utils/trajectory_utils.hpp
+++ include/utils/trajectory_utils.hpp
@@ -4,7 +4,10 @@
 
 #ifndef TRAJECTORY_UTILS_HPP
 #define TRAJECTORY_UTILS_HPP
+
+
 #include <pangolin/pangolin.h>
+
 #include <Eigen/Core>
 #include <bits/stdc++.h>
 
@@ -47,9 +50,71 @@ namespace motion::utils {
         return poses;
     };
 
+    constexpr std::int32_t SLEEP_TIME = 5000;
+
     inline auto draw_trajectories_open_gl_context(
         PosesVector poses = std::get<PosesVector>(read_trajectory_from_disk(resources::STR_TRAJECTORY_LOCATION))
         ) -> void {
+
+        const std::string WINDOW_NAME = "Trajectories";
+        pangolin::CreateWindowAndBind(WINDOW_NAME, 1024, 768);
+
+        // if (GLenum err = glewInit(); GLEW_OK != err) {
+        //     /* Problem: glewInit failed, something is seriously wrong. */
+        //     std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        //     throw std::runtime_error("Error initializing GLEW");
+        // }
+        // std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+        );
+
+        pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
+
+        while (pangolin::ShouldQuit() == false) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            d_cam.Activate(s_cam);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glLineWidth(2);
+            for (size_t i = 0; i < poses.size(); i++) {
+                Eigen::Vector3d Ow = poses[i].translation();
+                Eigen::Vector3d Xw = poses[i] * (0.01 * Eigen::Vector3d(1, 0, 0));
+                Eigen::Vector3d Yw = poses[i] * (0.01 * Eigen::Vector3d(0, 1, 0));
+                Eigen::Vector3d Zw = poses[i] * (0.01 * Eigen::Vector3d(0, 0, 1));
+                glBegin(GL_LINES);
+                glColor3f(1.0, 0.0, 0.0);
+                glVertex3d(Ow[0], Ow[1], Ow[2]);
+                glVertex3d(Xw[0], Xw[1], Xw[2]);
+                glColor3f(0.0, 1.0, 0.0);
+                glVertex3d(Ow[0], Ow[1], Ow[2]);
+                glVertex3d(Yw[0], Yw[1], Yw[2]);
+                glColor3f(0.0, 0.0, 1.0);
+                glVertex3d(Ow[0], Ow[1], Ow[2]);
+                glVertex3d(Zw[0], Zw[1], Zw[2]);
+                glEnd();
+            }
+
+            for (size_t i = 0; i < poses.size(); i++) {
+                glColor3f(0.0, 0.0, 0.0);
+                glBegin(GL_LINES);
+                auto p1 = poses[i], p2 = poses[i+1];
+                glVertex3d(p1.translation()[0], p1.translation()[1], p1.translation()[2]);
+                glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+                glEnd();
+            }
+
+
+            pangolin::FinishFrame();
+            usleep(SLEEP_TIME); //
+        }
 
     }
 
