a6ee3b9ae00c57223ac4bf4dd6cbae52b4b16217
mbero_akoko <mberoakoko@gmail.com>
2025-08-01 23:44:08 +0200

    minor changes to visual odometry intro.

--- include/visual_odometry/visual_odometry_intro.hpp
+++ include/visual_odometry/visual_odometry_intro.hpp
@@ -10,7 +10,34 @@
 #include <opencv2/highgui.hpp>
 #include <utility>
 
+#include "sophus/common.hpp"
+
 namespace visual_odometry::feature_extraction {
+
+    using OptPtrFeatureExtractor = std::optional<cv::Ptr<cv::FeatureDetector>>;
+
+    struct OrbTag{};
+    struct SiftTag{};
+    struct BriefTag{};
+
+    class ExtractorFactory {
+        using OrbPtr = cv::Ptr<cv::ORB>;
+        using SiftPtr = cv::Ptr<cv::SIFT>;
+    public:
+        template<typename Tag, typename ... Args>
+        static auto of(Args&&... args) -> OptPtrFeatureExtractor {
+            if constexpr (std::is_same_v<Tag, OrbTag>) {
+                return std::make_unique<std::optional<OrbPtr>>(std::forward<Args>(args)...);
+            } else if constexpr (std::is_same_v<Tag, SiftTag>) {
+                return std::make_unique<std::optional<SiftPtr>>(std::forward<Args>(args)...);
+            } else {
+                static_assert(!std::is_same_v<Tag, OrbTag> or !std::is_same_v<Tag, SiftTag>, "Error unsupported tag used !");
+                return nullptr;
+            }
+        }
+    };
+
+
     class BinaryFeatureExtractor {
         cv::Mat image_1_;
         cv::Mat image_2_;
@@ -18,7 +45,9 @@ namespace visual_odometry::feature_extraction {
         std::vector<cv::KeyPoint> keypoints_2_;
         cv::Mat descriptor_1_, descriptor_2_;
         cv::Ptr<cv::FeatureDetector> detector_ = cv::ORB::create();
+        // cv::Ptr<cv::FeatureDetector> detector_ = ExtractorFactory::of<FeatureTag>().value();
         cv::Ptr<cv::DescriptorExtractor> descriptor_extractor_ = cv::ORB::create();
+        // cv::Ptr<cv::DescriptorExtractor> descriptor_extractor_ = ExtractorFactory::of<FeatureTag>().value();
         cv::Ptr<cv::DescriptorMatcher> matcher_ = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE_HAMMING);
 
     public:
@@ -30,10 +59,24 @@ namespace visual_odometry::feature_extraction {
             
             descriptor_extractor_->compute(image_1_, keypoints_1_, descriptor_1_);
             descriptor_extractor_->compute(image_2_, keypoints_2_, descriptor_2_);
-            cv::Mat output_image:
+            cv::Mat output_image;
             cv::drawKeypoints(image_1_, keypoints_1_, output_image, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
             return output_image;
         }
+
+        [[nodiscard]]
+        auto match_features(const float& threshold = 30 ) const -> std::vector<cv::DMatch> {
+            assert(!descriptor_1_.empty() or descriptor_2_.empty());
+            std::vector<cv::DMatch> matches_;
+            matcher_->match(descriptor_1_, descriptor_2_, matches_);
+            const auto [min_elem, max_elem ] = std::ranges::minmax(matches_, [](const cv::DMatch& a, cv::DMatch& b) {return a.distance < b.distance;});
+            return matches_
+                |   std::views::filter([min_elem, threshold](const cv::DMatch& match)
+                                {return match.distance <= std::max(2*min_elem.distance, threshold);})
+                | std::ranges::to<std::vector<cv::DMatch>>();
+        }
     };
+
+
 }
 #endif //VISUAL_ODOMETRY_INTRO_HPP
